f85bd745a3f51c3d8eafbe63b4295094abdfb1cf
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-16 20:08:16 +0100

    Add migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  # get 'root/index'
+  root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
