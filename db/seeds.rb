9ca891d9ffb8dd999289f8ffbec2ae0386df0f78
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-17 23:53:22 +0100

    Add the seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# db/seeds.rb
+
+Message.create!(text: "Asalaam alaikum")
+Message.create!(text: "Hello!")
+Message.create!(text: "Hola!")
+Message.create!(text: "Bonjour")
+Message.create!(text: "Guten tag")
+Message.create!(text: "konnichiwa")
+Message.create!(text: "namaste")
+Message.create!(text: "merhaba")
